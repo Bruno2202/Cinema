dc01b1798f3c828d093f13df135ae2a5bac48e4f
Bruno2202 <brunoterribile2005@gmail.com>
2023-07-30 19:50:48 -0300

    Cinema

--- SCRIPT/script.js
+++ SCRIPT/script.js
@@ -25,29 +25,31 @@ Função para realizar a reserva de assentos.
 Os assentos reservador ficam salvos em uma matriz 12X16.
 */
 function reservar() {
-    linha = prompt("Informe a linha");
-    if (linha > 0 || linha < 0) {
+    linha = prompt("Informe a linha (Ex: A, B, C...)");
+    if (linha !== 'A' && linha !== 'B' && linha !== 'C' && linha !== 'D' && linha !== 'E' && linha !== 'F' && linha !== 'G' && linha !== 'H' && linha !== 'I' && linha !== 'J' && linha !== 'K' && linha !== 'L') { 
         alert("Não é uma entrada válida");  
-    }
-    wordToNumber();
-
-    var fileira = prompt("informe a fileira");
-    var fileiraConvert = fileira - 1;
-    if (fileira <= 0 || fileira >= 17) {
-        alert("Não é uma entrada válida");
-    }
-    
-    if (mCinemaL[linhaConvert][fileiraConvert] == 1) {
-        alert("Este assento já está reservado");
     } else {
-        conjunto = linha + fileira;
-        alert(`Assento ${conjunto} reservado com sucesso!`)
-        document.getElementById(conjunto).style.backgroundColor = 'red';
-        mCinemaL[linhaConvert][fileiraConvert] = 1;
-        console.log(`${conjunto} = ${mCinemaL[linhaConvert][fileiraConvert]}`);
+        //Função para converter letras em números para serem usados na matirz.
+        wordToNumber();
 
-        //Função para calcular a quantidade de assentos e informar ao usuário.
-        calcAssenstos();
+        var fileira = prompt("Informe a filiera (Ex: 1, 2, 3...)");
+        var fileiraConvert = fileira - 1;
+        if (fileira <= 0 || fileira >= 17) {
+            alert("Não é uma entrada válida");
+        } else {
+            if (mCinemaL[linhaConvert][fileiraConvert] == 1) {
+                alert("Este assento já está reservado");
+            } else {
+                conjunto = linha + fileira;
+                alert(`Assento ${conjunto} reservado com sucesso!`)
+                document.getElementById(conjunto).style.backgroundColor = 'red';
+                mCinemaL[linhaConvert][fileiraConvert] = 1;
+                console.log(`${conjunto} = ${mCinemaL[linhaConvert][fileiraConvert]}`);
+        
+                //Função para calcular a quantidade de assentos e informar ao usuário.
+                calcAssenstos();
+            }
+        }
     }
 }
 
@@ -55,29 +57,31 @@ function reservar() {
 Função para realizar o cancelamento de reservas.
 */
 function cancelar() {
-    linha = prompt("Informe a linha");
-    if (linha > 0 || linha < 0) {
+    linha = prompt("Informe a linha (Ex: A, B, C...)");
+    if (linha !== 'A' && linha !== 'B' && linha !== 'C' && linha !== 'D' && linha !== 'E' && linha !== 'F' && linha !== 'G' && linha !== 'H' && linha !== 'I' && linha !== 'J' && linha !== 'K' && linha !== 'L') {
         alert("Não é uma entrada válida");  
-    }
-    wordToNumber();
-
-    var fileira = prompt("informe a fileira");
-    var fileiraConvert = fileira - 1;
-    if (fileira <= 0 || fileira >= 17) {
-        alert("Não é uma entrada válida");
-    }
-
-    if (mCinemaL[linhaConvert][fileiraConvert] == 0) {
-        alert("Este assento ainda não foi reservado")
     } else {
-        conjunto = linha + fileira;
-        alert(`Assento ${conjunto} cancelado com sucesso!`)
-        document.getElementById(conjunto).style.backgroundColor = '#00158e';
-        mCinemaL[linhaConvert][fileiraConvert] = 0;
-        console.log(`${conjunto} = ${mCinemaL[linhaConvert][fileiraConvert]}`);
-    }
+        //Função para converter letras em números para serem usados na matirz.
+        wordToNumber();
 
-    //Função para calcular a quantidade de assentos e informar ao usuário.
-    calcAssenstos();
+        var fileira = prompt("Informe a filiera (Ex: 1, 2, 3...)");
+        var fileiraConvert = fileira - 1;
+        if (fileira <= 0 || fileira >= 17) {
+            alert("Não é uma entrada válida");
+        } else {
+            if (mCinemaL[linhaConvert][fileiraConvert] == 0) {
+                alert("Este assento ainda não foi reservado")
+            } else {
+                conjunto = linha + fileira;
+                alert(`Assento ${conjunto} cancelado com sucesso!`)
+                document.getElementById(conjunto).style.backgroundColor = '#00158e';
+                mCinemaL[linhaConvert][fileiraConvert] = 0;
+                console.log(`${conjunto} = ${mCinemaL[linhaConvert][fileiraConvert]}`);
+            }
+        
+            //Função para calcular a quantidade de assentos e informar ao usuário.
+            calcAssenstos();
+        }
+    }
 }
 
